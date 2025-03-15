bc2afd531044da99758b78e742f41e3f6b35119d
WillKirkmanM <WillKirkmanMoller@gmail.com>
2025-03-15 21:32:33 +0000

    Feat(main.rs): Initialise Interrupts on Start

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 #![no_std]
 #![no_main]
+#![feature(abi_x86_interrupt)]
 
 use core::panic::PanicInfo;
 mod vga;
 mod window_manager;
+mod interrupts;
+mod keyboard;
+mod command;
 
 use vga::Color;
 use window_manager::{Window, WINDOW_MANAGER};
@@ -11,6 +15,10 @@ use window_manager::{Window, WINDOW_MANAGER};
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     vga::WRITER.lock().clear_screen();
+    
+    interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
     
     let window1 = Window::new(5, 3, 30, 10, "Main Window", Color::Cyan);
     let window2 = Window::new(40, 5, 35, 8, "System Info", Color::Green);
@@ -35,11 +43,9 @@ pub extern "C" fn _start() -> ! {
     window2.print_at(1, 3, "Status: Running");
     
     let window3 = WINDOW_MANAGER.lock().get_window(id3).unwrap();
-    window3.print_at(1, 1, "$ echo \"Hello World!\"");
-    window3.print_at(1, 2, "Hello World!");
+    window3.print_at(1, 1, "$ ");
     
     loop {}
-
 }
 
 #[panic_handler]
